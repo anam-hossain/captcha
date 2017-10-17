2d6c705cfc5e561a1681421fa4f15aa81c1d0cf7
Anam hossain <anam@hotmail.com.au>
2017-10-17 21:40:04 +1100

    Load Site key dynamically

--- src/ServiceProvider/CaptchaServiceProvider.php
+++ src/ServiceProvider/CaptchaServiceProvider.php
@@ -1,6 +1,7 @@
 <?php
 namespace Anam\Captcha\ServiceProvider;
 
+use Anam\Captcha\SiteKeyNotFoundException;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
@@ -25,9 +26,12 @@ class CaptchaServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Blade::directive('captcha', function ($siteKey) {
+        Blade::directive('captcha', function ($siteKey = null) {
+            
+            $siteKey = $this->loadSiteKey($siteKey);
+
             return  "<script src='https://www.google.com/recaptcha/api.js'></script>".
-                    "<div class='g-recaptcha' data-sitekey='{{ $siteKey }}'></div>";
+                    "<div class='g-recaptcha' data-sitekey='{ $siteKey }'></div>";
         });
     }
 
@@ -39,5 +43,22 @@ class CaptchaServiceProvider extends ServiceProvider
     public function provides()
     {
         return [Captcha::class];
+    }
+
+    /**
+     * load reCAPTCHA Site key
+     * 
+     * @param  string  $siteKey
+     * @return string
+     */
+    public function loadSiteKey($siteKey = null)
+    {
+        if ($siteKey) return $siteKey;
+
+        if (env('RECAPTCHA_SITE_KEY')) {
+            return env('RECAPTCHA_SITE_KEY');
+        }
+
+        throw new SiteKeyNotFoundException;
     }
 }
