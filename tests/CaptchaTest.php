8f5a83d16e49918b71ca58a693f7941a229e2dff
Anam hossain <anam@hotmail.com.au>
2017-10-21 16:06:12 +1100

    Test verify return error on missing input response

--- tests/CaptchaTest.php
+++ tests/CaptchaTest.php
@@ -3,14 +3,26 @@
 namespace Anam\Captcha\Tests;
 
 use Anam\Captcha\Captcha;
+use Illuminate\Http\Request;
 
 class CaptchaTest extends TestCase
 {
     /**
      * @expectedException \Anam\Captcha\SecretNotFoundException
      */
-    public function testExceptionThrownOnInvalidSecret($secret = '')
+    public function testExceptionThrownOnInvalidSecret()
     {
-        $captcha = new Captcha($secret);
+        $captcha = new Captcha();
+    }
+
+    public function testVerifyReturnsErrorOnMissingInputResponse()
+    {
+        $captcha = new Captcha('secret');
+
+        $response = $captcha->check(new Request);
+
+        $this->assertFalse($response->isVerified());
+
+        $this->assertEquals('missing-input-response', $response->errors()->first());
     }
 }
