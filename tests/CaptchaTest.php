33f603c39d5dc41683d2a4aab6d65e3fd3c4afef
Anam hossain <anam@hotmail.com.au>
2017-10-21 21:33:34 +1100

    Add tests to verify the Captcha request

--- tests/CaptchaTest.php
+++ tests/CaptchaTest.php
@@ -2,6 +2,9 @@
 
 namespace Anam\Captcha\Tests;
 
+use Mockery;
+use ReCaptcha\ReCaptcha;
+use ReCaptcha\Response;
 use Anam\Captcha\Captcha;
 use Illuminate\Http\Request;
 
@@ -24,5 +27,56 @@ class CaptchaTest extends TestCase
         $this->assertFalse($response->isVerified());
 
         $this->assertEquals('missing-input-response', $response->errors()->first());
+    }
+
+    public function testSuccessfulResponseRequest()
+    {
+        $request = Mockery::mock(Request::class);
+        $request->allows()->input('g-recaptcha-response')->andReturn('xyz');
+
+        $request->allows()->ip()->andReturn('127.0.0.1');
+
+        $captcha = Mockery::mock(Captcha::class)->makePartial();
+        $captcha->allows()->verify($request->input('g-recaptcha-response'), $request->ip())
+            ->andReturn(new Response(true));
+
+        $captcha->check($request);
+
+        $this->assertTrue($captcha->isVerified());
+
+    }
+
+    public function testUnsuccessfulResponseRequest()
+    {
+        $request = Mockery::mock(Request::class);
+        $request->allows()->input('g-recaptcha-response')->andReturn('xyz');
+
+        $request->allows()->ip()->andReturn('127.0.0.1');
+
+        $captcha = Mockery::mock(Captcha::class)->makePartial();
+        $captcha->allows()->verify($request->input('g-recaptcha-response'), $request->ip())
+            ->andReturn(new Response(false));
+
+        $captcha->check($request);
+
+        $this->assertFalse($captcha->isVerified());
+
+    }
+
+    public function testHostname()
+    {
+        $request = Mockery::mock(Request::class);
+        $request->allows()->input('g-recaptcha-response')->andReturn('xyz');
+
+        $request->allows()->ip()->andReturn('127.0.0.1');
+
+        $captcha = Mockery::mock(Captcha::class)->makePartial();
+        $captcha->allows()->verify($request->input('g-recaptcha-response'), $request->ip())
+            ->andReturn(new Response(true, [], $request->ip()));
+
+        $captcha->check($request);
+
+        $this->assertEquals('127.0.0.1', $captcha->hostname());
+
     }
 }
