c4629b46a91c3e02c642121f45d1dee3d198b4f9
Michael Ducharme <mducharme@gmail.com>
2019-08-22 20:52:33 -0700

    Insecure captcha defaults removed

--- config/captcha.php
+++ config/captcha.php
@@ -8,11 +8,10 @@ return [
     |--------------------------------------------------------------------------
     |
     | Here you may provide the site key for google's recaptcha. 
-    | A default option is provided that is given for testing purposes by google.
     |
     */
 
-    'site_key' => env('RECAPTCHA_SITE_KEY', '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI'),
+    'site_key' => env('RECAPTCHA_SITE_KEY'),
 
     /*
     |--------------------------------------------------------------------------
@@ -20,10 +19,9 @@ return [
     |--------------------------------------------------------------------------
     |
     | Here you may provide the secret key for google's recaptcha. 
-    | A default option is provided that is given for testing purposes by google.
     |
     */
  
-    'secret' => env('RECAPTCHA_SECRET', '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe'),
+    'secret' => env('RECAPTCHA_SECRET'),
 
 ];
