9cfe4381448a23898630c4e6ca039e5190cb07f2
Michael Ducharme <mducharme@gmail.com>
2019-08-23 22:08:12 -0700

    Fixed one last indent problem

--- src/ServiceProvider/CaptchaServiceProvider.php
+++ src/ServiceProvider/CaptchaServiceProvider.php
@@ -81,7 +81,7 @@ class CaptchaServiceProvider extends ServiceProvider
         if (config('captcha.site_key')) {
             return config('captcha.site_key');
         } elseif (env('RECAPTCHA_SITE_KEY')) {
-        	return env('RECAPTCHA_SITE_KEY');
+            return env('RECAPTCHA_SITE_KEY');
         }
 
         throw new SiteKeyNotFoundException;
